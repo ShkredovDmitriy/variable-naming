35974e3ecafbe851cb38feb1451f3fc372ea1908
Shkredov Dmitriy <shkredovdmitriy@gmail.com>
2023-05-10 09:36:17 +0300

    feat: add js variable example

---
+++ index.js
@@ -0,0 +1,40 @@
+// SIMPLE
+const userUuid = "27068e98-0aec-4f47-a14d-e442783dd510";
+const userName = "Dmitriy";
+const userLastName = "Shkredov";
+const userSurName = "Stepanovich";
+const userBirthDay = new Date("October 13, 1984 02:00:00");
+
+// OBJECTS
+const user = {
+  uuid: "27068e98-0aec-4f47-a14d-e442783dd510",
+  name: "Dmitriy",
+  lastName: "Shkredov",
+  surName: "Stepanovich",
+  birthDay: new Date("October 13, 1984 02:00:00"),
+};
+
+// ARRAYS
+const userlist = [
+  {
+    uuid: "27068e98-0aec-4f47-a14d-e442783dd510",
+    name: "Dmitriy",
+    lastName: "Shkredov",
+    surName: "Stepanovich",
+    birthDay: new Date("October 13, 1984 02:00:00"),
+  },
+  {
+    uuid: "27068e98-0aec-4f47-a14d-e442783dd511",
+    name: "Alex",
+    lastName: "Vishnevskiy",
+    surName: "Nikolaevich",
+    birthDay: new Date("May 24, 1988 10:00:00"),
+  },
+  {
+    uuid: "27068e98-0aec-4f47-a14d-e442783dd512",
+    name: "Evgeniy",
+    lastName: "Kalinin",
+    surName: "Petrovich",
+    birthDay: new Date("Desember 18, 1982 17:00:00"),
+  },
+];
